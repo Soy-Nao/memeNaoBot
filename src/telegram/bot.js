22e4c41ccccdc52f1d972168439ac4725d901da5
Nao <loscanalesdenao@gmail.com>
2023-03-10 17:37:20 -0500

    texto de inicio

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -3,4 +3,10 @@ const TelegramBot = require("node-telegram-bot-api");
 
 const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
+bot.on("message", (msg) => {
+    if (msg.text.toString().toLowerCase() === "/start") {
+      bot.sendMessage(msg.chat.id, "¡Hola! Soy un bot de memes para coneversaciones, me puedes usar en cualquier chat de Telegram. /n Para iniciarme en un chat solo escribes '@memenaobot ' y se cargaran todos los memes, para encontrar rápidamente un meme, puedes buscarlo por alguna palabra que esté en el meme. /n ¿No está el meme que buscas? puedes subir tus memes, por el momento el unico modo es q me escribas indicando que quieres subir memes para darte el acceso @Soy_Nao");
+    }
+  });
+
 module.exports = bot;
