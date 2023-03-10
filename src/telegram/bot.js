c946e7d3ed6010299b3848d9cc92452724312626
Nao <loscanalesdenao@gmail.com>
2023-03-10 17:45:30 -0500

    saltos de linea

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -5,7 +5,7 @@ const bot = new TelegramBot(process.env.TOKEN, { polling: true });
 
 bot.on("message", (msg) => {
     if (msg.text.toString().toLowerCase() === "/start") {
-      bot.sendMessage(msg.chat.id, "¡Hola! Soy un bot de memes para coneversaciones, me puedes usar en cualquier chat de Telegram. /n Para iniciarme en un chat solo escribes '@memenaobot ' y se cargaran todos los memes, para encontrar rápidamente un meme, puedes buscarlo por alguna palabra que esté en el meme. /n ¿No está el meme que buscas? puedes subir tus memes, por el momento el unico modo es q me escribas indicando que quieres subir memes para darte el acceso @Soy_Nao");
+      bot.sendMessage(msg.chat.id, "¡Hola! Soy un bot de memes para coneversaciones. \n Me puedes usar en cualquier chat dentro de Telegram. \n Para iniciarme en un chat solo escribes '@memenaobot ' y se cargaran todos los memes, para encontrar rápidamente un meme, puedes buscarlo por alguna palabra que esté en la imagen. \n ¿No está el meme que buscas? \n Puedes subir tus memes, por el momento el unico modo es q me escribas indicando que quieres subir memes para darte el acceso @Soy_Nao");
     }
   });
 
