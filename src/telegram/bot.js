1254b50045daf0d8e14dba5a0555926fd613e13e
Soy_Nao <105759184+Soy-Nao@users.noreply.github.com>
2023-04-05 09:31:22 -0500

    webhooks

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -1,6 +1,17 @@
-
 const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: false });
+const app = express();
+app.use(bodyParser.json());
+
+const bot = new TelegramBot(process.env.TOKEN, {
+  webhook: {
+    port: process.env.PORT,
+    host: process.env.HOST,
+  },
+});
+
+bot.setWebHook(`${process.env.WEBHOOK_BASE_URL}/${bot.token}`);
 
 module.exports = bot;
